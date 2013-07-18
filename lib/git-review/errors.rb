8dfa7557359146d63204de293e5ef58a8eed1469
xystushi <xystushi@gmail.com>
2013-07-18 21:56:33 +0800

    Move up Errors by one level.

--- lib/git-review/errors.rb
+++ lib/git-review/errors.rb
@@ -1,34 +1,32 @@
 module GitReview
 
-  module Errors
-
-    class AuthenticationError < StandardError
-      def message
-        "You provided the wrong username/password, please try again.\n"
-      end
+  class AuthenticationError < StandardError
+    def message
+      "You provided the wrong username/password, please try again.\n"
     end
+  end
 
-    class InvalidGitRepositoryError < StandardError
-      def message
-        "It is not a valid git repository or doesn't have a valid remote url."
-      end
+  class InvalidGitRepositoryError < StandardError
+    def message
+      "It is not a valid git repository or doesn't have a valid remote url."
     end
+  end
 
-    # A custom error to raise, if we know we can't go on.
-    class UnprocessableState < StandardError
-      def message
-        "Execution of git-review stopped."
-      end
+  # A custom error to raise, if we know we can't go on.
+  class UnprocessableState < StandardError
+    def message
+      'Execution of git-review stopped.'
     end
+  end
 
-    class InvalidArgumentError < StandardError
+  class InvalidArgumentError < StandardError
 
+  end
+
+  class InvalidRequestIDError < StandardError
+    def message
+      'Please specify a valid request ID.'
     end
-
-    class InvalidRequestIDError < StandardError
-
-    end
-
   end
 
 end
